8a51361acaa797a26cbab8bec5b962d8545b0e71
Onur Ayçiçek <onur.m.aycicek@gmail.com>
2022-12-10 05:12:20 +0300

    uncomment call main function in __main__.py

---
+++ packages/telegram/crawl/__main__.py
@@ -0,0 +1,31 @@
+import os
+import sys
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+
+from dotenv import load_dotenv
+
+from helper.validation import validation
+from helper.response import error as response_error, success as response_success
+
+from helper.telegram import Bot
+
+load_dotenv()
+
+def make_crawl(args):
+   print("Crawling...")
+   app = Bot()
+   return app.get_last_posts_telegram(args["channels"])
+   
+
+def main(args):
+      # VALIDATION ---------------------------------
+      valid=validation(args)
+      if not valid["status"]:
+        return response_error(valid["message"])
+      # --------------------------------------------
+      response = make_crawl(args)
+      return response_success(response)
+
+
+if __name__ == "__main__":
+  print(main({"channels":["channel_user_name"]}))
